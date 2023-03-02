461110765e8384ff677274d6ad07ce5f8e1276e2
ydw <7224ydw@gmail.com>
2023-03-02 17:14:48 +0900

    테스트

---
+++ src/api/index.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+const userapi = axios.create({});
+
+//리턴값이 프로미스
+async function getUser() {
+  const res = await userapi.get("https://reqres.in/api/users?page=2");
+  return res.data.data;
+}
+
+export { getUser };
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ul>
+      <template v-for="(item, i) in user">
+        <li :key="i">{{ item }}</li>
+      </template>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { getUser } from "@/api/index.js";
 
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      user: [],
+    };
+  },
+  async created() {
+    this.user = await getUser();
   },
 };
 </script>
